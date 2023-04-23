4e77c212900a265c5423c62d4902739cf3d26906
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-04-23 05:16:33 -0400

    Add `clean` flag to reset virtual environment

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,11 +2,11 @@
 import os
 
 
-def main() -> None:
+def main(clean: bool = False) -> None:
     """Setup an environment for the ChatGPT Telegram bot project.
 
     Args:
-        dev (bool, optional): Setup development environment. Defaults to False.
+        clean (bool, optional): Clean the environment. Defaults to False.
     """
 
     # set working directory to the directory of this project
@@ -16,15 +16,25 @@ def main() -> None:
     python = os.path.join(venv, "bin", "python3")
     req = os.path.join(os.getcwd(), "requirements.txt")
 
+    if os.path.exists(venv) and clean:
+        # remove virtual environment
+        os.system(f"rm -rf {venv}")
+
     # create virtual environment
     os.system(f"python3 -m venv {venv}")
     # update package manager
     os.system(f"{python} -m pip install --upgrade pip")
-    # install dependencies
-    os.system(f"{python} -m pip install -r {req}")
+    # install dependencies, upgrade if already installed
+    os.system(f"{python} -m pip install -r {req} --upgrade")
 
     print("\nsetup complete")
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--clean", action="store_true",
+                        help="perform a clean setup")
+    args = parser.parse_args()
+    main(args.clean)
