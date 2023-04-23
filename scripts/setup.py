3445f59dc367ed25eb1df0bb3b5a00056de360f4
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-04-23 01:53:16 -0400

    Rework setup

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 import os
 
-REQUIREMENTS = "requirements"
-"""Path to the requirements files directory."""
 
-
-def main(dev: bool = False) -> None:
+def main() -> None:
     """Setup an environment for the ChatGPT Telegram bot project.
 
     Args:
@@ -17,30 +14,17 @@ def main(dev: bool = False) -> None:
     # set paths to virtual environment, requirements, and token
     venv = os.path.join(os.getcwd(), ".venv")
     python = os.path.join(venv, "bin", "python3")
-    req_dir = os.path.join(os.getcwd(), REQUIREMENTS)
-    local_env = os.path.join(os.getcwd(), "config", ".env")
+    req = os.path.join(os.getcwd(), "requirements.txt")
 
     # create virtual environment
     os.system(f"python3 -m venv {venv}")
     # update package manager
     os.system(f"{python} -m pip install --upgrade pip")
     # install dependencies
-    req = os.path.join(req_dir, "development.txt" if dev else "common.txt")
     os.system(f"{python} -m pip install -r {req}")
-    # load environment variables
-    with open(local_env, "w") as f:
-        f.write(f"TOKEN={os.environ.get('TELEGRAM_BOT_TOKEN')}\n")
 
-    print("\nSetup complete.")
+    print("\nsetup complete")
 
 
 if __name__ == "__main__":
-    import argparse
-
-    # parse arguments
-    parser = argparse.ArgumentParser("setup")
-    parser.add_argument("-d", "--dev", action="store_true",
-                        help="setup a development environment"
-                        )
-    args = parser.parse_args()
-    main(args.dev)
+    main()
---
+++ scripts/start.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import logging
+import os
+import sys
+from datetime import datetime
+
+# set working directory to the directory of this project
+os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+# add the project directory to the path
+sys.path.append(os.getcwd())
+
+
+def main(restore: bool = False, log: bool = False) -> None:
+    """Instantiates and runs the app. This function sets up logging and
+    checks the validity of the configured Telegram bot token.
+
+    Args:
+        restore (bool, optional): Whether to load the database from backup.
+        log (bool, optional): Whether to log to a file. Defaults to console.
+    """
+
+    if log:  # set up logging to file
+        logs_dir = os.path.join(os.getcwd(), "logs")
+        os.makedirs(logs_dir, exist_ok=True)
+        file = os.path.join(logs_dir, f'{datetime.now():%y%m%d_%H%M%S}.log')
+    else:  # log to console
+        file = None
+    logging.basicConfig(
+        filename=file,
+        level=logging.INFO,
+        # format='%(asctime)s[%(levelname)s] (%(name)s) - %(message)s'
+        format='[%(levelname)s] %(message)s (%(name)s)'
+    )
+
+    try:  # initialize the database and start the bot
+        import database.core as db
+        from chatgpt_bot import bot
+
+        db.start()
+        db.restore() if restore else None
+        bot.run()
+        db.stop()
+    except Exception as e:
+        print(f"\033[0;31m{'error:'}\033[0m {e}")
+        exit(1)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--restore", action="store_true",
+                        help="load database from backup")
+    parser.add_argument("-l", "--log", action="store_true",
+                        help="log to a file")
+    args = parser.parse_args()
+    main(args.log)
