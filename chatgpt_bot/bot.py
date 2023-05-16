b32a49b5101fc3caa84bb83f48a0b3fd6a69842a
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-05-16 19:55:11 -0400

    Add default bot behavior and better error handling

--- chatgpt_bot/bot.py
+++ chatgpt_bot/bot.py
@@ -3,8 +3,9 @@ manages the bot's lifecycle and tunneling updates to the handlers."""
 
 import secrets
 
-from telegram.constants import MessageEntityType
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.constants import MessageEntityType, ParseMode
+from telegram.ext import (Application, CommandHandler, Defaults,
+                          MessageHandler, filters)
 
 from chatgpt_bot import BOT_TOKEN, DEV, WEBHOOK, WEBHOOK_ADDR, WEBHOOK_PORT
 from chatgpt_bot.handlers import *
@@ -14,11 +15,21 @@ def run():
     """Run the bot."""
 
     # setup the bot
-    logger.info("starting telegram bot...")
-    app = Application.builder().token(BOT_TOKEN).build()
-    _setup(app)
+    defaults = Defaults(  # setup default settings
+        parse_mode=ParseMode.HTML,
+        allow_sending_without_reply=True,
+        quote=True, block=False
+    )
+    app = (  # setup the application
+        Application.builder()
+        .token(BOT_TOKEN)
+        .defaults(defaults)
+        .build()
+    )
+    _setup_handlers(app)
 
     # start the bot
+    logger.info("starting telegram bot...")
     if not DEV:
         app.run_webhook(
             listen=WEBHOOK_ADDR, port=WEBHOOK_PORT,
@@ -30,31 +41,32 @@ def run():
     logger.info("telegram bot has stopped")
 
 
-def _setup(app: Application):
+def _setup_handlers(app: Application):
     """Setup the bot's update handlers."""
+
     # add error/command handlers
     app.add_error_handler(error_handler)
-    app.add_handler(CommandHandler('delete', delete_history, block=False))
-    app.add_handler(CommandHandler('usage', send_usage, block=False))
-    app.add_handler(CommandHandler('start', dummy_callback, block=False))
-    app.add_handler(CommandHandler('sys', get_sys, block=False))
-    app.add_handler(CommandHandler('edit', edit_sys, block=False))
-    app.add_handler(CommandHandler('cancel', cancel_reply, block=False))
+    app.add_handler(CommandHandler('delete', delete_history, block=True))
+    app.add_handler(CommandHandler('usage', send_usage))
+    app.add_handler(CommandHandler('start', dummy_callback))
+    app.add_handler(CommandHandler('sys', get_sys))
+    app.add_handler(CommandHandler('edit', edit_sys))
+    app.add_handler(CommandHandler('cancel', cancel_reply))
 
     # add message handlers
     app.add_handler(MessageHandler(
-        filters.ChatType.PRIVATE, block=False,
+        filters.ChatType.PRIVATE,
         callback=private_callback
     ))
     app.add_handler(MessageHandler(
-        filters.Entity(MessageEntityType.MENTION), block=False,
+        filters.Entity(MessageEntityType.MENTION),
         callback=mention_callback
     ))
     app.add_handler(MessageHandler(
-        filters.ALL, block=False,
+        filters.ALL,
         callback=store_update
     ))
     app.add_handler(MessageHandler(
-        filters.ATTACHMENT, block=False,
+        filters.ATTACHMENT,
         callback=check_file
     ))
